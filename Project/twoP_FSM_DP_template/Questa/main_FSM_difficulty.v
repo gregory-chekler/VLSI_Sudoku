cc74377b4e1dffa29a3da5dc5a1343558bf723d3
Hong-Ye Wang <hw62@zorite.clear.rice.edu>
2024-02-27 00:37:29 -0600

    added difficulty selection to main FSM

--- Project/twoP_FSM_DP_template/Questa/main_FSM_difficulty.v
+++ Project/twoP_FSM_DP_template/Questa/main_FSM_difficulty.v
@@ -6,20 +6,25 @@
 // 		 start signal to data path dp.
 // 		 Waits then for done signal from the datapath.
 //-----------------------------------------------------
-module main_FSM (clka, clkb, restart, enter, state, dp_check, solved, won);
+module main_FSM (clka, clkb, restart, enter, state, dp_check, solved, difficulty, won);
 //-------------Input Ports-----------------------------
-input   clka, clkb, restart, enter, solved, difficulty, rand_idx[7:0];
+input   clka, clkb, restart, enter, solved;
+input   difficulty[1:0];
 //-------------Output Ports----------------------------
-output  won, state[2:0];
+output  won, state[3:0];
 //-------------Input ports Data Type-------------------
 wire    clka, clkb, restart, load, done;
+wire    difficulty[1:0];
 //-------------Output Ports Data Type------------------
-reg     ridx_a, ridx_b, dp_check, fiil_flag[7:0];
+reg     dp_check, ridx_a, ridx_b;
+reg     fill_flag[7:0];
+
 //——————Internal Constants--------------------------
 parameter SIZE = 4;
-parameter easy_mode = 2'b00, medium_mode = 2'b01, hard_mode_a = 2'b10; hard_mode_b = 2'b11;
+parameter easy_mode = 2'b01, medium_mode = 2'b10, hard_mode = 2'b11;
 parameter REG_INP  = 4'b0000, GUESS = 4'b0001, CHECK = 4'b0010, WRONG = 4'b0011, FIN = 4'b0100;
-parameter SET_DIFF = 4'b0101, EASY = 4'b0110, MEDIUM = 4'b0111, HARD = 4'b1000, FILL_BOARD = 4'b1001; 
+parameter EMPTY = 4'b0101, SET_DIFF = 4'b0110, EASY = 4'b0111, MEDIUM = 4'b1000, HARD = 4'b1001; 
+
 //-------------Internal Variables---------------------------
 reg   [SIZE-1:0]          state;    	// Initial FSM state reg and then after
 					// processing new output FSM state reg
@@ -31,10 +36,10 @@ reg   [SIZE-1:0]          next_state; 	// Temporary reg to hold next state to
 assign temp_state = fsm_function(state, enter, solved);
 //----------Function for Combinational Logic to read inputs -----------
 function [SIZE-1:0] fsm_function;
-  input  [SIZE-1:0] state ;
+  input  [SIZE-1:0] state;
   input enter;
   input solved;
-
+  input difficulty;
 case(state)
    REG_INP: begin
     if (enter) begin
@@ -71,7 +76,30 @@ case(state)
   FIN: begin
           fsm_function = FIN;
          end
-   default: fsm_function = REG_INP;
+  EMPTY: begin
+          fsm_function = SET_DIFF;
+        end
+  SET_DIFF: begin
+              if (difficulty == easy_mode) begin
+                fsm_function = EASY;
+              end else if (difficulty == medium_mode) begin
+                fsm_function = MEDIUM;
+              end else if (difficulty == hard_mode) begin
+                fsm_function = HARD;
+              end else begin
+                fsm_function = SET_DIFF;
+              end
+            end
+  EASY: begin
+              fsm_function = REG_INP; 
+        end
+  MEDIUM: begin
+              fsm_function = REG_INP; 
+          end
+  HARD: begin
+              fsm_function = REG_INP; 
+        end
+  default: fsm_function = REG_INP;
   endcase
 endfunction
 //----------Seq Logic-----------------------------
@@ -106,12 +134,46 @@ begin : OUTPUT_LOGIC
   FIN: begin
           state <= next_state;
           dp_check <= 1'b0;
+        end
+  EMPTY: begin
+          state <= next_state;
+          dp_check <= 1'b0;        
+        end
+  SET_DIFF: begin
+          state <= next_state;
+`         dp_check <= 1'b0;
+          ridx_a <= 1'b1;
+          ridx_a <= 1'b1;
+          fill_flag <= 8'b00000000;
+            end
+  EASY: begin
+          state <= next_state;
+`         dp_check <= 1'b0;
+          ridx_a <= 1'b0;
+          ridx_a <= 1'b0;
+          fill_flag <= 8'b00001111; // for testing, four random hints in row 1 and 2
+        end
+  MEDIUM: begin
+          state <= next_state;
+`         dp_check <= 1'b0;
+          ridx_a <= 1'b0;
+          ridx_a <= 1'b0;
+          fill_flag <= 8'b00000111; // for testing, three random hints in row 1 and 2
           end
+  HARD: begin
+          state <= next_state;
+`         dp_check <= 1'b0;
+          ridx_a <= 1'b0;
+          ridx_a <= 1'b0;
+          fill_flag <= 8'b00000011; // for testing, two random hints in row 1 and 2
+        end                 
  default: begin
           state <= next_state;
           start <= 1'b0;
+          ridx_a <= 1'b0;
+          ridx_b <= 1'b0;
+          fill_flag <= 8'b00000000;
          end
   endcase
 end // End Of Block OUTPUT_LOGIC
-
 endmodule // End of Module main_FSM
